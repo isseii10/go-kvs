f7d4ab7c44efb7fa136f223cabb9e3059ce937ab
isseii10 <t1l.g2.o11g@gmail.com>
2024-06-08 19:37:31 +0900

    storeのinterface定義

---
+++ store/store.go
@@ -0,0 +1,25 @@
+package store
+
+import (
+	"context"
+	"io"
+)
+
+type Txn interface {
+	Get(ctx context.Context, key []byte) ([]byte, error)
+	Put(ctx context.Context, key []byte, value []byte) error
+	Delete(ctx context.Context, key []byte) error
+	Exists(ctx context.Context, key []byte) (bool, error)
+}
+
+type Store interface {
+	Get(ctx context.Context, key []byte) ([]byte, error)
+	Put(ctx context.Context, key []byte, value []byte) error
+	Delete(ctx context.Context, key []byte) error
+	Exists(ctx context.Context, key []byte) (bool, error)
+	Snapshot() (io.ReadWriter, error)
+	Restore(buf io.Reader) error
+	// Txn トランザクション用
+	Txn(ctx context.Context, f func(ctx context.Context, txn Txn) error) error
+	Close() error
+}
